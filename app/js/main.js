6e11b92e2d0579219d12617c37d0ba59f5e8a443
Alex noteAir <ktotaam@mail.ru>
2020-08-21 14:28:43 +0500

    save

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ $(function () {
     $('.promo-section__slider').slick({
         dots: true,
         speed: 800,
+        autoplay: true,
         prevArrow: "<button class='promo-section__arrow'><img src='../images/icons/arrow-left.svg'></button>",
         nextArrow: "    <button class='promo-section__arrow promo-section__arrow--right'><img" +
             " src='../images/icons/arrow-right.svg'></button>",
