b60dc08ebba2a8a7a87fde7817cc1c48bb40dbd8
Alexander Grobovsky <ktotaam@mail.ru>
2020-09-10 12:30:28 +0500

    save

--- app/js/main.js
+++ app/js/main.js
@@ -8,10 +8,19 @@ $(function () {
     $('.promo-section__slider').slick({
         dots: true,
         speed: 800,
-        autoplay: true,
+        // autoplay: true,
         prevArrow: "<button class='promo-section__arrow'><img src='../images/icons/arrow-left.svg'></button>",
         nextArrow: "    <button class='promo-section__arrow promo-section__arrow--right'><img" +
             " src='../images/icons/arrow-right.svg'></button>",
+        responsive: [
+            {
+                breakpoint: 781,
+                settings: {
+                    arrows: false,
+                    // centerMode: true,
+                }
+            },
+        ]
     });
 
     $('.products__slider').slick({
